daacc559f5b9e25638d60f283ac13173288f90c2
Won Wook SONG <wsong0512@gmail.com>
2015-10-23 13:50:53 +0200

    add published

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,26 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Three steps for migration:
+# 1. Change your models (in models.py).
+# 2. Run python manage.py makemigrations to create migrations for those changes
+# 3. Run python manage.py migrate to apply those changes to the database.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
